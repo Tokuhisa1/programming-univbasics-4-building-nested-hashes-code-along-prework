1106d571303c9287522e3f5c4f3e4013555199c2
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 15:38:29 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -9,7 +9,7 @@ def fourth_challenge
         {
           :name => "Benvolio",
           :age => "17",
-          :attitude => 
+          :attitude => "worried"
         },
         {
           :name => ,
