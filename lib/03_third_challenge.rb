fdbb1c083c95b6da37ad5344f72938e08983e9a2
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 15:29:09 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -4,26 +4,36 @@ def third_challenge
       :patriarch => {
         #your key/value pairs here
         :name => "Lord Montague",
-        :age of "53"
+        :age => "53"
       },
       :matriarch => {
         #your key/value pairs here
-        },
+        :name => "Lord Montague",
+        :age => "53"
+      },
       :hero => {
         #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        },
+        :name => "Lord Montague",
+        :age => "53"
+      },
       :matriarch => {
         #your key/value pairs here
-        },
+        :name => "Lord Montague",
+        :age => "53"
+      },
       :heroine => {
         #your key/value pairs here
-        },
+        :name => "Lord Montague",
+        :age => "53"
+      },
       :heroine_friends => []
    }
   }
